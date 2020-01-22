26d04823b59ddbf75fc48fde6f18d8da279042ba
何标 <hebiao@hdfax.com>
2020-01-22 17:19:23 +0800

    init

---
+++ src/main/java/com/carl/controller/SysGeneratorController.java
@@ -0,0 +1,69 @@
+package com.carl.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.carl.service.SysGeneratorService;
+import com.carl.utils.PageUtils;
+import com.carl.utils.Query;
+import com.carl.utils.R;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 代码生成器
+ * 
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2016年12月19日 下午9:12:58
+ */
+@Controller
+@RequestMapping("/sys/generator")
+public class SysGeneratorController {
+	@Autowired
+	private SysGeneratorService sysGeneratorService;
+	
+	/**
+	 * 列表
+	 */
+	@ResponseBody
+	@RequestMapping("/list")
+	public R list(@RequestParam Map<String, Object> params){
+		//查询列表数据
+		Query query = new Query(params);
+		List<Map<String, Object>> list = sysGeneratorService.queryList(query);
+		int total = sysGeneratorService.queryTotal(query);
+		
+		PageUtils pageUtil = new PageUtils(list, total, query.getLimit(), query.getPage());
+		
+		return R.ok().put("page", pageUtil);
+	}
+	
+	/**
+	 * 生成代码
+	 */
+	@RequestMapping(value = "/code",method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public void code(@RequestParam(value = "tables") String tables, HttpServletResponse response) throws IOException{
+		 String[] tableNames = new String[]{};
+		tableNames = JSON.parseArray(tables).toArray(tableNames);
+		
+		byte[] data = sysGeneratorService.generatorCode(tableNames);
+		
+		response.reset();  
+        response.setHeader("Content-Disposition", "attachment; filename=\"carl.zip\"");
+        response.addHeader("Content-Length", "" + data.length);  
+        response.setContentType("application/octet-stream; charset=UTF-8");  
+  
+        IOUtils.write(data, response.getOutputStream());  
+	}
+}
